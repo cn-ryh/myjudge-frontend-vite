7b4ef7a57cc2ab9895bb3923c17b0c3292037c80
cn_ryh <15543677192@163.com>
2024-05-04 21:26:07 +0800

    再重试

--- docs/src/.vuepress/config.ts
+++ docs/src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-    base: "/",
+    base: "/docs/",
     lang: "zh-CN",
     title: "11 Online Judge Document",
     description: "11 Online Judge 文档",
