5efd41e8e9b367cdc793f1e429fa43ee92546c85
cn_ryh <15543677192@163.com>
2024-07-27 10:11:09 +0800

    update admin

--- src/pages/admin/problem-new.vue
+++ src/pages/admin/problem-new.vue
@@ -39,18 +39,16 @@ function newProblem() {
             <span> 题目编号 </span>
             <input placeholder="自动分配" id="problemIdInputer" disabled>
         </div>
-        <Tabs default-active-key="1">
-            <TabPanel key="1" title="管理">
-                <div>
-                    <span>题库：</span>
-                    <Select v-model="type" style="width:280px;margin-left: 2%;" placeholder="请选择题库">
-                        <Option :value="`B`">入门题库</Option>
-                        <Option :value="`P`">主题库</Option>
-                        <Option :value="`M`">精英题库</Option>
+        <Tabs :default-value="1">
+            <TabPanel :value="1" label="题目信息">
+                <div style="height: 32px;display: flex;">
+                    <span style="flex: 0 1 6rem; font-size: 1.1rem; ">题库：</span>
+                    <Select v-model=" type" style="width:280px;" placeholder="请选择题库">
+                        <Option label="入门题库" :value="`B`">入门题库</Option>
+                        <Option label="主题库" :value="`P`">主题库</Option>
+                        <Option label="精英题库" :value="`M`">精英题库</Option>
                     </Select>
                 </div><br>
-
-
                 <div style="height: 32px;display: flex;">
                     <span style="flex: 1 1 7rem; font-size: 1.1rem;">题目名称：</span>
                     <input v-model="title" id="title" style="width: 100%;outline: 0;font-size: large;" />
@@ -63,27 +61,28 @@ function newProblem() {
                         placeholder="请输入题目描述，支持Markdown。"></textarea>
                 </div>
                 <br>
-                <div>
-                    <span>题目难度</span>
+                <div style="display: flex;flex-direction: row;">
+                    <span style="flex: 0 1 6rem; font-size: 1.1rem;">题目难度</span>
                     <Select v-model="diff" style="width:280px;margin-left: 2%;" placeholder="请选择题目难度">
-                        <Option :value="1">普及 T1</Option>
-                        <Option :value="2">普及 T2</Option>
-                        <Option :value="3">普及 T3</Option>
-                        <Option :value="4">普及 T4 / 提高 T1</Option>
-                        <Option :value="5">提高 T2</Option>
-                        <Option :value="6">提高 T3</Option>
-                        <Option :value="7">提高 T4</Option>
-                        <Option :value="8">省选</Option>
-                        <Option :value="9">NOI</Option>
-                        <Option :value="10">NOI+</Option>
-                        <Option :value="0">暂无评定</Option>
+                        <Option label="普及 T1" :value="1">普及 T1</Option>
+                        <Option label="普及 T2" :value="2">普及 T2</Option>
+                        <Option label="普及 T3" :value="3">普及 T3</Option>
+                        <Option label="普及 T4 / 提高 T1" :value="4">普及 T4 / 提高 T1</Option>
+                        <Option label="提高 T2" :value="5">提高 T2</Option>
+                        <Option label="提高 T3" :value="6">提高 T3</Option>
+                        <Option label="提高 T4" :value="7">提高 T4</Option>
+                        <Option label="省选" :value="8">省选</Option>
+                        <Option label="NOI" :value="9">NOI</Option>
+                        <Option label="NOI+" :value="10">NOI+</Option>
+                        <Option label="暂无评定" :value="0">暂无评定</Option>
                     </Select>
-                    <Button @click="newProblem" theme="primary">
-                        新建题目
-                    </Button>
                 </div>
+                <br />
+                <Button @click="newProblem" theme="primary">
+                    新建题目
+                </Button>
             </TabPanel>
-            <TabPanel key="2" title="数据上传" disabled @click="() => { $notification.error(`需要先创建题目`) }">
+            <TabPanel value="2" label="数据上传" disabled @click="() => { $notification.error(`需要先创建题目`) }">
             </TabPanel>
         </Tabs>
     </div>
